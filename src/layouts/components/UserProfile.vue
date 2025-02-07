8e6a773a92fef56aaf39166c1c999f418f63a50c
vals25-mg <rvalisoa3@gmail.com>
2025-02-07 15:41:23 +0300

    first commit

---
+++ src/layouts/components/UserProfile.vue
@@ -0,0 +1,80 @@
+<script setup>
+import avatar1 from '@images/avatars/avatar-1.png'
+</script>
+
+<template>
+  <VBadge
+    dot
+    location="bottom right"
+    offset-x="3"
+    offset-y="3"
+    bordered
+    color="success"
+  >
+    <VAvatar
+      class="cursor-pointer"
+      color="primary"
+      variant="tonal"
+    >
+      <VImg :src="avatar1" />
+
+      <!-- SECTION Menu -->
+      <VMenu
+        activator="parent"
+        width="230"
+        location="bottom end"
+        offset="14px"
+      >
+        <VList>
+          <!-- 👉 User Avatar & Name -->
+          <VListItem>
+            <template #prepend>
+              <VListItemAction start>
+                <VBadge
+                  dot
+                  location="bottom right"
+                  offset-x="3"
+                  offset-y="3"
+                  color="success"
+                >
+                  <VAvatar
+                    color="primary"
+                    variant="tonal"
+                  >
+                    <VImg :src="avatar1" />
+                  </VAvatar>
+                </VBadge>
+              </VListItemAction>
+            </template>
+
+            <VListItemTitle class="font-weight-semibold">
+              John Doe
+            </VListItemTitle>
+            <VListItemSubtitle>Admin</VListItemSubtitle>
+          </VListItem>
+
+
+          <!-- 👉 Profile -->
+
+
+          <!-- Divider -->
+          <VDivider class="my-2" />
+
+          <!-- 👉 Logout -->
+          <VListItem to="/login">
+            <template #prepend>
+              <VIcon
+                class="me-2"
+                icon="tabler-logout"
+                size="22"
+              />
+            </template>
+
+            <VListItemTitle>Se déconnecter</VListItemTitle>
+          </VListItem>
+        </VList>
+      </VMenu>
+      <!-- !SECTION -->
+    </VAvatar>
+  </VBadge>
+</template>
